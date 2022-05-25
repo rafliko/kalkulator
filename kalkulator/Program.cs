4cf5deeb19b0e61e36bcb400aea8b26a99e21148
rafliko <raflikpalka@gmail.com>
2022-05-25 09:22:52 +0200

    add menu

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -6,7 +6,35 @@ namespace kalkulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a, b;
+            string d;
+            do
+            {
+                Console.WriteLine("Wpisz 1 liczbę:");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Wpisz 2 liczbę:");
+                b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Podaj działanie(+, -, *, /, NWD, z - zakończ):");
+                d = Console.ReadLine();
+                switch (d)
+                {
+                    case "+":
+                        Matma.dodawanie(a,b);
+                        break;
+                    case "-":
+                        Matma.odejmowanie(a, b);
+                        break;
+                    case "*":
+                        Matma.mnozenie(a, b);
+                        break;
+                    case "/":
+                        Matma.dzielenie(a, b);
+                        break;
+                    case "NWD":
+                        Matma.nwd(a, b);
+                        break;
+                }
+            } while (d!="z");
         }
     }
 }
